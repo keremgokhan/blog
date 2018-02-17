bcf12913aa04b8d23dfc1cbf37ae2a4850daf2eb
Kerem Gokhan <mail@keremgokhan.net>
2018-02-17 19:28:18 +0000

    Routing 2.0

--- lib/Blog.pm
+++ lib/Blog.pm
@@ -12,24 +12,73 @@ sub startup {
     # $self->mode('development');
     
     $self->plugin('authentication' => {
-	'autoload_user' => 1,
-	'session_key' => 'blog',
-	'load_user' => \&Blog::Authentication::load_user,
-	'validate_user' => \&Blog::Authentication::validate_user,
-	'current_user_fn' => 'user',
+	'autoload_user'         => 1,
+	'session_key'           => 'blog',
+	'load_user'             => \&Blog::Authentication::load_user,
+	'validate_user'         => \&Blog::Authentication::validate_user,
+	'current_user_fn'       => 'user'
     });
+
+    my $route_map = {
+        '/' => {
+            method      => 'GET',
+            name        => 'index',
+            controller  => 'index',
+            action      => 'index'
+        },
+        '/post/:id' => {
+            method      => 'GET',
+            name        => 'show_post',
+            controller  => 'post',
+            action      => 'index'
+        },
+        '/post' => {
+            method      => 'POST',
+            name        => 'create_post',
+            controller  => 'post',
+            action      => 'create'
+        },
+        '/admin' => {
+            method      => 'GET',
+            name        => 'admin',
+            controller  => 'admin',
+            action      => 'index'
+        },
+        '/admin/login' => {
+            method      => 'POST',
+            name        => 'login',
+            controller  => 'admin',
+            action      => 'user_login'
+        },
+        '/admin/logout' => {
+            method      => 'GET',
+            name        => 'logout',
+            controller  => 'admin',
+            action      => 'user_logout'
+        },
+        '/admin/create' => {
+            method      => 'GET',
+            name        => 'create',
+            controller  => 'admin',
+            action      => 'create_post'
+        }
+    };
+
+    $self->load_routes( $route_map );
+}
+
+sub load_routes {
+    my ( $self, $route_map ) = @_;
 
     my $r = $self->routes;
 
-    $r->get('/')->to('index#index')->name('index');
-    $r->get('/post/:id')->to('post#index')->name('show_post');
-    $r->post('/post')->to('post#create')->name('create_post');
+    foreach my $route ( keys %{$route_map} ) {
+        my $data        = $route_map->{$route};
+        my $method      = delete $data->{method} // '';
 
-    $r->get('/admin')->to('admin#index')->name('admin');
-    $r->post('/admin/login')->to('admin#user_login')->name('login');
-    $r->get('/admin/logout')->to('admin#user_logout')->name('logout');
-
-    $r->get('/admin/create')->to('admin#create_post')->name('create');
+        $r->get($route)->to($data)      if $method eq 'GET';
+        $r->post($route)->to($data)     if $method eq 'POST';
+    }
 }
 
 1;
